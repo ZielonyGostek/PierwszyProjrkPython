c7c6a38014dc9daaa28819cc7a8bbebdff12f09a
ZielonyGostek <145969306+ZielonyGostek@users.noreply.github.com>
2023-11-03 21:28:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+liczba =int(input('Podaj liczbę od 1 do 5'))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if liczba == 1:
+    print('JEDEN')
+elif liczba == 2:
+    print('DWA')
+elif liczba == 3:
+    print('TRZY')
+elif liczba == 4:
+    print('CZTERY')
+elif liczba == 5:
+    print('PIĘĆ')
+else:
+    print('Czytaj uważniej polecenia!')
+    print('koniec')
